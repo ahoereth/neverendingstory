8cc9c1b58a22031df5af4429dc75a1b016dd3a12
Alexander Höreth <a.hoereth@gmail.com>
2014-12-04 16:58:00 +0200

    Watch next upcoming election date for changes.

--- server/server.js
+++ server/server.js
@@ -2,4 +2,21 @@
 
 Meteor.startup(function () {
   Meteor.call('stories/watchNextElection');
+
+  // Watch the election date of the next story coming up because if it changes
+  // we need to check the closest upcoming election again. Mostly
+  // interesting when debugging.
+  var storyCursor = Stories.find({
+    nextElectionDate: {$exists: true}
+  }, {
+    sort  : {nextElectionDate: 1},
+    fields: {nextElectionDate: 1},
+    limit : 1
+  }).observeChanges({
+    changed: function(id, fields) {
+      if (! _.isUndefined(fields.nextElectionDate)) {
+        Meteor.call('stories/watchNextElection')
+      }
+    }
+  })
 });
